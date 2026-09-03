9a047092833a6eefea2842e46e7c3882a22b2659
Dimiodez <andrew.dimio@gmail.com>
2026-09-03 16:51:43 -0500

    Add complete 6v6 result history

--- pickems-app/season-data.js
+++ pickems-app/season-data.js
@@ -22,6 +22,42 @@ window.UFL_SEASON = {
     ['IB',13,2,0,11,18,49,-31,6], ['TAB',13,1,0,12,19,69,-50,3]
   ],
   weeks: [
+    { week: 1, date: 'Tue 21 Jul, 11:00 PM EDT', scheduledAt: 1784689200, matches: [
+      [12,'GOTH','NL',5,0],[13,'PUM','TAB',3,1],[14,'IB','PAL',0,3],[15,'JAG','COM',2,3],[16,'HAM','BAY',5,2]
+    ]},
+    { week: 2, date: 'Thu 23 Jul, 11:00 PM EDT', scheduledAt: 1784862000, matches: [
+      [17,'NL','PUM',2,2],[18,'GOTH','IB',2,1],[19,'TAB','JAG',1,3],[20,'PAL','HAM',0,4],[21,'COM','BAY',6,1]
+    ]},
+    { week: 3, date: 'Tue 28 Jul, 11:00 PM EDT', scheduledAt: 1785294000, matches: [
+      [22,'IB','NL',0,5],[23,'JAG','PUM',0,1],[24,'HAM','GOTH',5,0],[25,'BAY','TAB',5,2],[26,'COM','PAL',5,4]
+    ]},
+    { week: 4, date: 'Thu 30 Jul, 11:00 PM EDT', scheduledAt: 1785466800, matches: [
+      [27,'NL','JAG',1,2],[28,'IB','HAM',0,6],[29,'PUM','BAY',5,1],[30,'GOTH','COM',3,3],[31,'TAB','PAL',0,8]
+    ]},
+    { week: 5, date: 'Tue 04 Aug, 11:00 PM EDT', scheduledAt: 1785898800, matches: [
+      [32,'HAM','NL',4,1],[33,'BAY','JAG',1,5],[34,'COM','IB',5,0],[35,'PAL','PUM',2,4],[36,'TAB','GOTH',1,4]
+    ]},
+    { week: 6, date: 'Thu 06 Aug, 11:00 PM EDT', scheduledAt: 1786071600, matches: [
+      [37,'NL','BAY',3,2],[38,'HAM','COM',3,1],[39,'JAG','PAL',6,1],[40,'IB','TAB',7,6],[41,'PUM','GOTH',1,1]
+    ]},
+    { week: 7, date: 'Tue 11 Aug, 11:00 PM EDT', scheduledAt: 1786503600, matches: [
+      [42,'COM','NL',3,3],[43,'PAL','BAY',1,3],[44,'TAB','HAM',0,8],[45,'GOTH','JAG',5,3],[46,'PUM','IB',3,0]
+    ]},
+    { week: 8, date: 'Thu 13 Aug, 11:00 PM EDT', scheduledAt: 1786676400, matches: [
+      [47,'NL','PAL',1,5],[48,'COM','TAB',5,2],[49,'BAY','GOTH',1,4],[50,'HAM','PUM',3,1],[51,'JAG','IB',7,2]
+    ]},
+    { week: 9, date: 'Tue 18 Aug, 11:00 PM EDT', scheduledAt: 1787108400, matches: [
+      [52,'TAB','NL',1,6],[53,'GOTH','PAL',4,3],[54,'PUM','COM',2,0],[55,'IB','BAY',2,3],[56,'JAG','HAM',1,3]
+    ]},
+    { week: 10, date: 'Thu 20 Aug, 11:00 PM EDT', scheduledAt: 1787281200, matches: [
+      [57,'NL','GOTH',0,3],[58,'TAB','PUM',1,8],[59,'PAL','IB',1,0],[60,'COM','JAG',5,0],[61,'BAY','HAM',1,3]
+    ]},
+    { week: 11, date: 'Tue 25 Aug, 11:00 PM EDT', scheduledAt: 1787713200, matches: [
+      [62,'PUM','NL',2,0],[63,'IB','GOTH',2,5],[64,'JAG','TAB',2,3],[65,'HAM','PAL',2,2],[66,'BAY','COM',3,6]
+    ]},
+    { week: 12, date: 'Thu 27 Aug, 11:00 PM EDT', scheduledAt: 1787886000, matches: [
+      [67,'NL','IB',0,3],[68,'PUM','JAG',7,1],[69,'GOTH','HAM',3,2],[70,'TAB','BAY',1,6],[71,'PAL','COM',2,2]
+    ]},
     { week: 13, date: 'Tue 01 Sep, 11:00 PM EDT', scheduledAt: 1788318000, matches: [
       [72,'JAG','NL',4,3],[73,'HAM','IB',3,1],[74,'BAY','PUM',3,2],[75,'COM','GOTH',4,1],[76,'PAL','TAB',4,0]
     ]},
